84b764c8c5e0d1a1d6713f03dcb2ba3e70be80ae
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-10-25 07:10:16 +0900

    lab1.4

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write(
+      "<body><form action = '/message' method='POST'><input name='message' type = 'text'><button type = 'submit'>send</button></input></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
