6416ad32788a500993b030f8c1eddafa9a108571
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-10-28 07:04:11 +0900

    2.5

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/", (req, res, next) => {
-  console.log("This always runs!");
   next();
 });
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware!");
-  res.send('<h1>The "Add Product" Page</h1>');
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="tittle"><button type="submit">Add Product</button></input></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
