c16a4df33aa15655fbcc9eaad39fe17525407296
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-10-28 06:52:34 +0900

    2.4

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const http = require("http");
 const express = require("express");
+
 const app = express();
 
-app.use((req,res, next)=>{
-    console.log('in the middleware')
-    next();
-})
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
+  next();
+});
 
-app.use((req, res, next)=>{
-    console.log('in another middleware')
-    res.send('<h1>Hello from Express!</h1>')
-})
-const server = http.createServer(app);
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleware!");
+  res.send('<h1>The "Add Product" Page</h1>');
+});
 
-server.listen(3000);
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express</h1>");
+});
 
-
-
+app.listen(3000);
