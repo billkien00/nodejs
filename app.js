803c9cfbfcb824d45df7ec091d25c59d49a7750a
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-10-28 05:21:08 +0900

    2.2

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const http = require("http");
 const express = require("express");
 const app = express();
-// const port = 3000
 
+app.use((req,res, next)=>{
+    console.log('in the middleware')
+    next();
+})
+
+app.use((req, res, next)=>{
+    console.log('in another middleware')
+})
 const server = http.createServer(app);
 
 server.listen(3000);
 
-// app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`)
-//   })
+
+
