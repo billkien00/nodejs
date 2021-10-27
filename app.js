2bd6300e6af7c2e0f5b8745751b412200b290c0c
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-10-28 07:42:11 +0900

    2.9

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const shopRouters = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRouters);
 app.use((req, res, next) => {
   res.status(404).send("<h1>Page not found</h1>");
