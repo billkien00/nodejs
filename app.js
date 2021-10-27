c9817d955526338d5e8978183feb58db748786bc
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-10-27 23:05:41 +0900

    2.1

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const http = require("http");
+const express = require("express");
+const app = express();
+// const port = 3000
 
-const routes = require("./routes");
-console.log(routes.someText);
-
-const server = http.createServer(routes.handler);
+const server = http.createServer(app);
 
 server.listen(3000);
+
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+//   })
