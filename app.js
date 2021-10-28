5b58ce862bb891b8706c44c0b7d8b677d1968aab
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-10-28 15:53:09 +0900

    2.14

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const adminRoutes = require("./routes/admin");
 const shopRouters = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRouters);
