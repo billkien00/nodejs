3acea332f3496920d103860c8912b2fc76d1c1e2
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-10-28 16:30:03 +0900

    3.1

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRouters = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRouters);
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
