58e62cda776fbd6552339c46462e8bec6503ede5
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-10-31 04:02:19 +0900

    3.10

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: ".hbs",
-  })
-);
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRouters = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-// trả về path của thư mục public1
+// trả về path của thư mục public
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminData.routes);
