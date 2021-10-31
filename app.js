343801152bba7179464b8b17d6a82d181ec820b9
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-11-01 06:48:35 +0900

    4.1

--- app.js
+++ app.js
@@ -7,14 +7,18 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRouters = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+//middleware để dùng dùng body(lấy dữ liệu sau khi submit có method là post) node 4.16
+// app.use(express.urlencoded()) gửi html lên sever
+// app.use(express.json()) gửi js lên sever (XMLHttpRequest, fetch, axios,....)
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // trả về path của thư mục public
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRouters);
 app.use((req, res, next) => {
   res.status(404).render("404", { pageTitle: "Page Not Found" });
