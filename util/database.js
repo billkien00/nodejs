e5a5a236b122511c17d011e974e33604ed327454
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-11-20 17:03:36 +0900

    5.3

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "node-complete",
-  password: "nodecomplete",
+  password: "beatboxermk1",
 });
 
 module.exports = pool.promise();
