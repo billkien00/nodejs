7ce5810611b29e64cfa98fe339c048b37101af2d
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-11-21 01:36:06 +0900

    5.8

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "beatboxermk1", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "beatboxermk1",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
