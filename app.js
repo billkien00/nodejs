31939ba1ef3f3a07adac49f5f988806a64b428bf
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-11-23 13:58:35 +0900

    5.10

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const errorController = require("./controllers/error");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -25,4 +25,14 @@ app.use("/admin", adminRoutes);
 app.use(shopRouters);
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// app.listen(3000);
