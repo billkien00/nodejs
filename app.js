6c3d710109ce7ce46d0dac3765ca6719b5bbf537
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-11-30 14:23:41 +0900

    5.18

--- app.js
+++ app.js
@@ -1,38 +1,58 @@
 const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const app = express();
-
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
-const shopRouters = require("./routes/shop");
+const shopRoutes = require("./routes/shop");
 
-//middleware để dùng dùng body(lấy dữ liệu sau khi submit có method là post) node 4.16
-// app.use(express.urlencoded()) gửi html lên sever
-// app.use(express.json()) gửi js lên sever (XMLHttpRequest, fetch, axios,..)
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// trả về path của thư mục public
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findById(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
-app.use(shopRouters);
+app.use(shopRoutes);
+
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
+  // .sync({ force: true })
   .sync()
   .then((result) => {
+    return User.findById(1);
     // console.log(result);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Max", email: "test@test.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
     console.log(err);
   });
-
-// app.listen(3000);
