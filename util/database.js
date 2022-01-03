48f0dafb6825851923e5578fa815661f7a89d297
billkien00 <89521288+billkien00@users.noreply.github.com>
2022-01-03 12:36:26 +0900

    done

--- util/database.js
+++ util/database.js
@@ -1,8 +1,29 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "beatboxermk1", {
-  dialect: "mysql",
-  host: "localhost",
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb://dbUser:beatboxermk1@cluster0-shard-00-00.dhypg.mongodb.net:27017,cluster0-shard-00-01.dhypg.mongodb.net:27017,cluster0-shard-00-02.dhypg.mongodb.net:27017/shop?ssl=true&replicaSet=atlas-nyrz8z-shard-0&authSource=admin&retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
