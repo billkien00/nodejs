de97d54b281997f5af75a7522b4e0b92f676d7b9
billkien00 <89521288+billkien00@users.noreply.github.com>
2022-01-20 14:25:11 +0900

    done

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/user");
 
 const app = express();
@@ -19,9 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("61d24621dbdcbc38e18b5200")
+  User.findById("61da59af6b018c0798308d73")
     .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch((err) => console.log(err));
@@ -32,6 +32,25 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    "mongodb://dbUser:beatboxermk1@cluster0-shard-00-00.dhypg.mongodb.net:27017,cluster0-shard-00-01.dhypg.mongodb.net:27017,cluster0-shard-00-02.dhypg.mongodb.net:27017/shop?ssl=true&replicaSet=atlas-nyrz8z-shard-0&authSource=admin&retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Kien",
+          email: "kien@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
